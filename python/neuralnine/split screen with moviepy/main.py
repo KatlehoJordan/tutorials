2ebeda4824547450739d24bdc6bccc78a0be5fea
D <david.gray.lassiter@gmail.com>
2022-07-22 10:59:44 +0200

    Add comments and vids with margins

--- python/neuralnine/split screen with moviepy/main.py
+++ python/neuralnine/split screen with moviepy/main.py
@@ -28,4 +28,19 @@ clip_4_vids_2_rows.write_videofile('4_vids_2_rows.mp4')
 # Then I could play the first one without a problem.
 # Attempts to play the other two led to "Server execution failed".
 # After this, trying to play the first or the original files also led to the same error.
-# Tried restarting computer.
+# Tried restarting computer .
+# That worked!
+# Noticed that clip3 and clip4 are largeer - probably due to size I downloaded from pixabay.
+
+# Adding a margin
+
+clip1_small_margin = VideoFileClip('Aerial - 18390.mp4').subclip(0, 0 + length).margin(1)
+clip2_small_margin = VideoFileClip('Fall - 23881.mp4').subclip(0, 0 + length).margin(1)
+
+clip1_large_margin = VideoFileClip('Aerial - 18390.mp4').subclip(0, 0 + length).margin(9)
+clip2_large_margin = VideoFileClip('Fall - 23881.mp4').subclip(0, 0 + length).margin(9)
+clip_2_vids_small_margin = clips_array([[clip1_small_margin, clip2_small_margin]])
+clip_2_vids_large_margin = clips_array([[clip1_large_margin, clip2_large_margin]])
+
+clip_2_vids_small_margin.write_videofile('2_vids_small_margin.mp4')
+clip_2_vids_large_margin.write_videofile('2_vids_large_margin.mp4')
